1c9acec4e29683ee6a3b7043150041f7b76d7df7
pussbb <pussbb@gmail.com>
2012-04-18 10:29:26 +0300

    getting how much app used memorry

--- qwebrender/main.cpp
+++ qwebrender/main.cpp
@@ -2,6 +2,12 @@
 #include <demon.h>
 #include <QSystemTrayIcon>
 #include <QPainter>
+#include <QDebug>
+
+#include <QFile>
+#include <QStringList>
+#include <QProcess>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -13,7 +19,30 @@ int main(int argc, char *argv[])
     p.drawText(10,15,QString::number(12));
     p.end();
     QSystemTrayIcon *tray = new QSystemTrayIcon();
-    ;
+    qDebug()<<QApplication::applicationPid();
+    int pid = QApplication::applicationPid();
+    QFile file(QString("/proc/%1/status").arg(pid));
+    file.open(QIODevice::ReadOnly);
+    QString content = file.readAll();
+    QString pattern = "%1:\\s+(\\d+)";
+    int total;
+    QStringList memoryVars = QStringList() << "VmSize"<< "VmHWM" << "VmRSS"
+                             << "VmData" << "VmLib" << "VmSwap";
+    foreach(QString key, memoryVars) {
+        QRegExp reg(pattern.arg(key));
+        int pos = reg.indexIn(content);
+        if ( pos > -1) {qDebug()<<reg.cap(1).toInt();
+            total += reg.cap(1).toInt();
+        }
+    }
+    if (total > 4874228) {
+        QString program = a.applicationFilePath();
+        QProcess *myProcess = new QProcess();
+        myProcess->start(a.applicationFilePath(), a.arguments());
+        myProcess->waitForStarted();
+        qDebug()<< myProcess->pid();
+        exit(0);
+    }
     tray->setIcon(img);
     tray->show();
 
